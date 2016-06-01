198764cfb52c2ced9b0c44acb0ef0e9930ae99a0
Puneeth U Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2016-06-01 14:14:02 -0500

    first commit

---
+++ models/Posts.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+var PostSchema = new mongoose.Schema({
+	title: String,
+	link: String,
+	upvotes: {
+		type: Number,
+		default: 0
+	},
+	comments: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Comment'
+	}]
+});
+
+PostSchema.methods.upvote = function(cb){
+	this.upvote += 1;
+	this.save(cb);
+}
+
+PostSchema.methods.downvote = function(cb){
+	this.upvote -= 1;
+	this.save(cb);
+}
+
+mongoose.model('Post', PostSchema);
